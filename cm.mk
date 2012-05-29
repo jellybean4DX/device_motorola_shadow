e87b4ad0755be69de6f843f49f9bf9202174a732
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-05-29 11:45:10 -0400

    Repush shadow tree

---
+++ cm.mk
@@ -0,0 +1,12 @@
+# Boot animation
+TARGET_BOOTANIMATION_NAME := vertical-480x854
+
+# Inherit device configuration
+$(call inherit-product, device/motorola/shadow/shadow.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := shadow
+PRODUCT_NAME := cm_shadow
+PRODUCT_BRAND := verizon
+PRODUCT_MODEL := DROIDX
+PRODUCT_MANUFACTURER := Motorola
