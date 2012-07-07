a676c19891f6a42d8d783f94022c42c63151dda4
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-07-06 23:45:20 -0300

    update pack-newboot.sh to work on every compile

--- prebuilt/etc/hijack-boot/pack-newboot.sh
+++ prebuilt/etc/hijack-boot/pack-newboot.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+cd $ANDROID_BUILD_TOP/device/motorola/shadow/prebuilt/etc/hijack-boot/
 zip -r hijack-boot.zip META-INF newboot
-mv hijack-boot.zip ../hijack-boot.zip
+mv hijack-boot.zip $OUT/system/etc/hijack-boot.zip
