e299ca0fadf73b1a858ca85e2a9ef9d26aade196
Brandon McAnsh <bmc08gt.teamb00sted@gmail.com>
2012-07-07 13:51:50 -0300

    Add if -f for hijack-boot.zip and notes

--- prebuilt/etc/hijack-boot/pack-newboot.sh
+++ prebuilt/etc/hijack-boot/pack-newboot.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
-cd $ANDROID_BUILD_TOP/device/motorola/shadow/prebuilt/etc/hijack-boot/
+# 2nd- Init Hijack-boot zipper
+# Will allow us to use a fresh copy of our hijack-boot zipped up
+# during compile instead of using a prebuilt one
+
+cd $ANDROID_BUILD_TOP/device/motorola/shadow/prebuilt/etc/
+
+if [ -f hijack-boot.zip ]; 
+    then rm -f hijack-boot.zip
+fi
+
+cd hijack-boot
 zip -r hijack-boot.zip META-INF newboot
 mv hijack-boot.zip ../hijack-boot.zip
